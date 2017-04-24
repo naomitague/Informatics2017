e4ae3a1392cc0a160976886acc57f48cc2a27845
naomitague <ctague@bren.ucsb.edu>
2017-04-24 12:57:52 -0700

    editting

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -1,6 +1,3 @@
 
 sunlight = read.table("sun.txt", header=T)
-par(mar=c(5,6,3,2))
-boxplot(sunlight$Kdown_direct~sunlight$month, 
-        ylab="Downwelling Solar\n kJ/m2/day",
-        xlab="month",col="orange")
+ls()
